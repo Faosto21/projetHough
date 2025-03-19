44f48fce0ad86aa333c5110c2b2bced069977741
typhonisos <158085072+typhonisos@users.noreply.github.com>
2025-03-19 15:41:17 +0100

    Update Hough.h

--- projetHough/projetHough/Hough.h
+++ projetHough/projetHough/Hough.h
@@ -1,13 +1,37 @@
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 struct Image{
-    std::vector<std::vector<int>> pixels;
+    std::vector<std::vector<std::vector<int>>> pixels;
     int nbColonnes;
     int nbLignes;
+    int maxColor;
 
-    Image():pixels(0),nbColonnes(0),nbLignes(0){
+    Image(const std::string& fichier){
+        std::ifstream file(fichier);
+        if (!file){
+            throw std::string "Problème lors de l'ouverture du fichier";
+            return;
+        }
+        std::string format;
+        file >>format;
+        if (format !="P3"){
+            throw std::string "Pas le bon format ";
+            return;
+        }
+        file>> nbColonnes >> nbLignes >> max Colors; // On récupère les caractéristiques de l'image
+        pixels.resize(nbLignes, std::vector<std::vector<int>>(nbColonnes, std::vector<int>(3)));
+        // On lit les données de chaque pixels
+        for (int i = 0; i < nbLignes; ++i) {
+            for (int j = 0; j < nbColonnes; ++j) {
+                file >> pixels[i][j][0] >> pixels[i][j][1] >> pixels[i][j][2]; // On lie dans l'ordre Rouge, Vert, Bleu
+            }
+        }
+
+        file.close();
+        std::cout << "Image chargée "<< std::endl;
     }
-    Image(std::vector<std::vector<int>> tableau): pixels(tableau),nbColonnes(tableau[0].size()),nbLignes(tableau.size()){}
 
 };
 
