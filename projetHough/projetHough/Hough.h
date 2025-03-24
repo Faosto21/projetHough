1594442ce60fe16921cfbbaa2d73f79b728ca01e
typhonisos <158085072+typhonisos@users.noreply.github.com>
2025-03-24 08:52:19 +0100

    Update Hough.h

--- projetHough/projetHough/Hough.h
+++ projetHough/projetHough/Hough.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 
+
 struct Image{
     std::vector<std::vector<std::vector<int>>> pixels;
     size_t nbColonnes;
@@ -37,7 +38,7 @@ struct Image{
         // On lit les données de chaque pixels
         for (size_t i = 0; i < nbLignes; ++i) {
             for (size_t j = 0; j < nbColonnes; ++j) {
-                file >> pixels[i][j][0] >> pixels[i][j][1] >> pixels[i][j][2]; // On lie dans l'ordre Rouge, Vert, Bleu
+                file >> pixels[i][j][0] >> pixels[i][j][1] >> pixels[i][j][2]; // On lit dans l'ordre Rouge, Vert, Bleu
             }
         }
 
@@ -48,15 +49,61 @@ struct Image{
 };
 
 struct Hough{
-    std::vector<std::vector<int>> points;
+    std::vector<std::vector<int>> compteur_droite;
+    int mmin=-10;
+    int mmax=10;
+    double step=0.1;
+    int nbstep=(mmax-mmin)/step;
 
+    // On teste des valeurs de m allant de -10 à 10
+    // b peut aller de -100 à 100
     Hough(const Image& image){
-      points.resize(image.nbLignes,std::vector<int>(image.nbColonnes, 0));
+        compteur_droite.resize(nbstep,std::vector<int>(nbstep, 0));
+        for(size_t i=0;i<image.nbLignes;i++){
+            for(size_t j=0;j<image.nbColonnes;j++){
+                if (Bord(image.pixels[i][j])){
+                    build_lines(i,j);
+                    }
+            }
+        }
     }
+
+    bool Bord(const std::vector<int>& pixel){
+        int intensite = (pixel[0]+pixel[1]+pixel[2])/3;
+        return intensite<128;
+    }
+    void build_lines(size_t x, size_t y){
+        for(int i=0;i<nbstep;i++){
+            double m=mmin+i*step;
+            double b=y-m*x;
+            int mindex=i;
+            int bindex=static_cast<int>(b + compteur_droite.size() / 2); // on remet l'indice dans [0,200]
+            if (bindex>=0 && bindex<compteur_droite.size()){
+                compteur_droite[mindex][bindex]++;
+            }
+        }
+    }
+    std::pair<int,int> trouver_droite(){  // Fonction qui trouve la droite avec le plus de vote dans l'image de Hough
+    int max_value=INT_MIN;
+    std::pair<int,int> droite={0,0};
+    for (int i=0;i<compteur_droite.size();i++){
+        for (int j=0;j<compteur_droite[0].size();j++){
+            int valeur=compteur_droite[i][j];
+            if (valeur>max_value){
+                max_value=valeur;
+                droite={mmin+i*step,j-compteur_droite.size() /2};
+            }
+        }
+    }
+    return  droite;
+    }
+
 };
 
 
 int main(){
     Image image("imageM1.ppm");
+    Hough hough(image);
+    std::cout<< hough.trouver_droite().first <<std::endl;
+
     return 0;
-}
