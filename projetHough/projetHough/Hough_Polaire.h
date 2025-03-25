fd6b7f5bbdb4c25697ad73dc242330d9886e6652
typhonisos <158085072+typhonisos@users.noreply.github.com>
2025-03-25 16:08:31 +0100

    Commmentating Hough_Polaire.h
    Deleted detecter_droite_proche ( already have regrouper_droite)

--- projetHough/projetHough/Hough_Polaire.h
+++ projetHough/projetHough/Hough_Polaire.h
@@ -13,27 +13,28 @@
 #include "Image.h"
 
 struct Hough_polaire{
-    std::vector<std::vector<int>> compteur_droite;
+    std::vector<std::vector<int>> compteur_droite; // compteur_droite[i][j] compte le nombre de sinusoïdales qui passent par le point (i,j)
     int rho_max;
     double step=0.1;
 
     Hough_polaire(const Image& image){
         int largeur=image.nbColonnes;
         int hauteur=image.nbLignes;
-        rho_max=static_cast<int>(std::floor(std::sqrt(std::pow(largeur,2)+std::pow(hauteur,2))));
+        rho_max=static_cast<int>(std::floor(std::sqrt(std::pow(largeur,2)+std::pow(hauteur,2)))); // Calcul du rho_max en fonction des dimensions de l'image
         std::cout << "rhomax =" << rho_max<<std::endl;
         int nb_step=2*rho_max/step;
         compteur_droite.resize(180,std::vector<int>(nb_step,0));
         for (size_t i=0;i<image.nbLignes;i++){
             for(size_t j=0;j<image.nbColonnes;j++){
                 if (Bord(image.pixels,j,i)){
-                    build_curve(i,j);
+                    build_curve(i,j);  // On 'dessine' les courbes uniquement pour les points considérés comme au bord
                 }
             }
         }
 
     }
     bool Bord(const std::vector<std::vector<std::vector<int>>>& pixels, size_t x, size_t y) {
+        // Fonction qui détermine si un point est au bord d'une droite à partir du gradient de couleur du point
         int hauteur = pixels.size();
         int largeur = pixels[0].size();
 
@@ -57,6 +58,7 @@ struct Hough_polaire{
     }
 
     void build_curve(size_t x, size_t y){
+        // Fonction qui 'dessine' la sinusoïdales associé au point (x,y) dans l'espace de Hough en ajoutant 1 à tout les points traversés par cette sinusoïdale
         std:: cout << "x :" << x << ", y:" << y << std::endl;
         for(int i=0;i<180;i++){
             double rho=x*std::cos(i*M_PI/180)+y*std::sin(i*M_PI/180);
@@ -66,7 +68,8 @@ struct Hough_polaire{
             }
         }
     }
-    std::vector<std::pair<double,double>> trouver_droites(int seuil){  // Fonction qui trouve les droites avec le plus de votes dans l'image de Hough
+    std::vector<std::pair<double,double>> trouver_droites(int seuil){ 
+        // Fonction qui trouve les points avec un nombre de votes au dessus d'un certain seuil dans l'espace de Hough, et donc les droites dans l'image initiale
         std::pair<double,double> droite={0,0};
         std::vector<std::pair<double,double>> vec_droites;
         double theta;
@@ -87,23 +90,6 @@ struct Hough_polaire{
 
 };
 
-std::vector<std::pair<double,double>> detect_droite_proche(std::vector<std::pair<double,double>> vec_droites,double epsilon_m,double epsilon_b){
-    std::vector<std::pair<double,double>> res;
-    bool check=false;
-    for (auto droite1 : vec_droites){
-        check=false;
-        for (auto droite2 : res){
-            if ((std::abs(droite1.first-droite2.first < epsilon_m)) && (std::abs(droite1.second-droite2.second))<epsilon_b){
-                check=true;
-                break;
-            }
-        }
-        if (!check){
-            res.push_back(droite1);
-        }
-    }
-    return res;
-}
 
 
 #endif /* Hough_Polaire_h */
